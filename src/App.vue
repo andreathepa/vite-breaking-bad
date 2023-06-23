c5e6d0aeb701787ae491a6ab69ac7b4dc1a5814b
andreathepa <andreadepa32@gmail.com>
2023-06-23 17:45:19 +0200

    bonus - loader created

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import AppHeader from './components/AppHeader.vue';
 import AppPokemonList from './components/AppPokemonList.vue';
 import { store } from './store.js';
 import axios from 'axios';
+import AppLoader from './components/AppLoader.vue';
 
 
 export default {
     components:{
       AppHeader,
-      AppPokemonList
+      AppPokemonList,
+      AppLoader
     },
     data(){
         return{
@@ -27,6 +29,7 @@ export default {
   <div>
     <AppHeader  class="text-light" title="Pokedex"/>
     <AppPokemonList />
+    <AppLoader />
   </div>
 </template>
 <style lang="scss">
---
+++ src/components/AppLoader.vue
@@ -0,0 +1,72 @@
+<script>
+export default {
+    
+}
+</script>
+<template lang="">
+    <div>
+        <div class="big_loader">
+            <div class="loader">
+                <div class="line"></div><div class="ballin"></div>
+            </div>
+
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+
+    .big_loader{
+        position: fixed;
+        top: 0;
+        right: 0;
+        width: 100vw;
+        height: 100vh;
+        background-color: rgb(255, 136, 0);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .loader {
+    border: 3px solid #000000;
+    border-radius: 50%;
+    background: rgb(246,246,246);
+    background: linear-gradient(0deg, rgba(246,246,246,1) 51%, rgba(237,58,26,1) 52%);
+    width: 120px;
+    height: 120px;
+    -webkit-animation: spin 2s linear infinite; /* Safari */
+    animation: spin 2s linear infinite;
+
+        .ballin{
+            border: 2px solid black;
+            width: 20px;
+            height: 20px;
+            border-radius: 50%;
+            position: absolute;
+            top:39%;
+            left:41%;
+            background-color: white;
+        }
+
+        .line{
+            width: 115px;
+            height: 1px;
+            border: 2px solid black;
+            position: absolute;
+            top: 45%;
+            left: 0%;
+        }
+    }
+
+    /* Safari */
+    @-webkit-keyframes spin {
+    0% { -webkit-transform: rotate(0deg); }
+    100% { -webkit-transform: rotate(360deg); }
+    }
+
+    @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+    }
+</style>
